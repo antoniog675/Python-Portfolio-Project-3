e19c053960b5666ee9577904e12c9df7ce890576
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-04-08 15:00:37 +0000

    Adding hidden and guess board for one player before implementing a computer board and guessing board

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 from random import randint
 
-PLAYER_BOARD = [[" "] * 6 for i in range(6)]
-COMPUTER_BOARD = [[" "] * 6 for i in range(6)]
+HIDDEN_BOARD = [[" "] * 6 for i in range(6)]
+GUESS_BOARD = [[" "] * 6 for i in range(6)]
 # PLAYER_GUESS_BOARD = [[" "] * 6 for i in range(6)]
 # COMPUTER_GUESS_BOARD = [[" "] * 6 for i in range(6)]
 LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5}
 
 def print_board(board):
     print("-" * 15)
-    if board == PLAYER_BOARD:
-        print("  User board")
-    else:
-        if board == COMPUTER_BOARD:
-            print("Computer board")
-
+    # if board == PLAYER_BOARD:
+    #     print(" Player board")
+    # else:
+    #     if board == PLAYER_GUESS_BOARD:
+    #         print("Computer board")
+    print("COMPUTER BOARD")
     print("-" * 15)
     print("  A B C D E F")
     print("  +-+-+-+-+-+")
@@ -25,20 +25,29 @@ def print_board(board):
 def place_ships(board):
     for ship in range(5):
         ship_row, ship_column = randint(0,5), randint(0,5)
-        while board[ship_row][ship_column] == 'X':
+        while board[ship_row][ship_column] == '@':
             ship_row, ship_column = randint(0,5), randint(0,5)
-        board[ship_row][ship_column] = 'X'
+        board[ship_row][ship_column] = '@'
         
 def user_guess(): 
     # Wrap in try and except to get valid input, will crash if nothin is entered.
-    row = input("Please enter a ship row 1-6: ")
-    while row not in '123456':
-        print('Please enter valid row')
-        row = input("Please enter a ship row 1-6: ")
-    column = input("Please enter a ship column A-F: ").upper()
-    while column not in 'ABCEDF':
-        print("Please enter a valid column: ").upper()
-    return int(row) -1, LETTERS_TO_NUMBERS[column]
+    while True:
+        try: 
+            row = input("Enter the row of the ship: ")
+            if row in '12345678':
+                row = int(row) - 1
+                break
+        except ValueError:
+            print('Enter a valid letter between A-H')
+    while True:
+        try: 
+            column = input("Enter the column of the ship: ").upper()
+            if column in 'ABCDEFGH':
+                column = LETTERS_TO_NUMBERS[column]
+                break
+        except KeyError:
+            print('Enter a valid letter between A-H')
+    return row, column
 
 def count_hit_ships(board):
     count = 0
@@ -48,11 +57,38 @@ def count_hit_ships(board):
                 count += 1
     return count
 
-place_ships(PLAYER_BOARD)
-turns = 10
-place_ships(COMPUTER_BOARD)
-print_board(PLAYER_BOARD)
-print_board(COMPUTER_BOARD)
+# place_ships(PLAYER_BOARD)
+# turns = 10
+# print_board(PLAYER_BOARD)
+# print_board(COMPUTER_BOARD)
+
+def play_game():
+    place_ships(HIDDEN_BOARD)
+    print_board(GUESS_BOARD)
+    user_guess()
+    turns = 5
+    while turns > 0:
+        print('Guess a battleship location')
+        row, column = place_ships()
+        if GUESS_BOARD[row][column] == "-":
+            print("You guessed that one already.")
+        elif HIDDEN_BOARD[row][column] == "X":
+            print("Hit")
+            GUESS_BOARD[row][column] = "X" 
+            turns -= 1  
+        else:
+            print("MISS!")
+            GUESS_BOARD[row][column] = "-"   
+            turns -= 1     
+        if count_hit_ships(GUESS_BOARD) == 5:
+            print("You win!")
+            break
+        print("You have " + str(turns) + " turns left")
+        if turns == 0:
+            print("You ran out of turns")
+    
+    
+
 
 
 def get_user_inputs():
@@ -64,15 +100,13 @@ def get_user_inputs():
     print(f"Welcome {user_name}, are you ready to play the game?")
     start_game = input("Enter 'Y' to begin or 'N' to exit: ").upper()
     if start_game == "Y":
-        print_board(PLAYER_BOARD)
+        play_game()
     elif start_game == "N":
         exit()
     else:
         print("Uh oh, please enter a valid input")
         get_user_inputs()
 
-# def run_game():
-#     print("Lets begin")
-
 print("Welcome to Battleships!\n")
-get_user_inputs()
+get_user_inputs() #This will call on the get_user_inputs() function and get user name
+                  #and if they're ready to play the game.
