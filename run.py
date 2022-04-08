3e0226c1adcf77790b3a92870b2ad9c90a64bd6c
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-04-08 10:05:15 +0000

    Added validation to get user to input Y or N and if anything else it will ask them to enter one of the two again

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_user_inputs():
+    print("Are you ready to play the game?")
+    start_game = input("Enter 'Y' to begin or 'N' to exit: ").upper()
+    if start_game == "Y":
+        run_game()
+    elif start_game == "N":
+        exit()
+    else:
+        print("Uh oh, please enter a valid input")
+        get_user_inputs()
+
+def run_game():
+    print("Lets begin")
+
+print("Welcome to Battleships!\n")
+get_user_inputs()
+
+
+    
+        
+
